0ea979ae273032b2c67013d77c95b789aa5bedd5
srikanth-vanam <B172320-@rgukt.ac.in>
2023-08-02 20:41:59 +0530

    added Expense-Items

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+    return (
+    <div>
+        <h3>Expense Items</h3>
+        <div>
+            <p>Food Rs 10</p>
+            <p>Petrol Rs 100</p>
+            <p>Movies Rs 200</p>
+        </div>
+    </div>
+);
+}
+
+export default ExpenseItem;
