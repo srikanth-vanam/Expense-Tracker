7fedfe933ce503e28ed1005d92e59bb81eb58d58
srikanth-vanam <B172320-@rgukt.ac.in>
2023-08-03 13:30:22 +0530

    added css file + Expense-Item with LocationOfExpenditure

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,19 @@
+import "./ExpenseItem.css";
 function ExpenseItem() {
-    return (
-    <div>
-        <h3>Expense Items</h3>
-        <div>
-            <p>Food Rs 10</p>
-            <p>Petrol Rs 100</p>
-            <p>Movies Rs 200</p>
-        </div>
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expensePrice = "$200";
+  const locationOfExpenditure = "Hyderabad";
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h4>{locationOfExpenditure}</h4>
+        <div className="expense-item__price"> {expensePrice}</div>
+      </div>
     </div>
-);
+  );
 }
 
 export default ExpenseItem;
