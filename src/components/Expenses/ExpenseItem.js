16a9c2165e26c69ce882777ff8c85ce6dd32f8cf
srikanth-vanam <B172320-@rgukt.ac.in>
2023-08-07 19:16:48 +0530

    added btn to change price to

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,22 @@
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
-  const deleteExpenseHandler = (e) => {
-    console.log("Deletes expense");
-    // delete the expense via DOM manipulation
+  const [price,setPrice]=useState(props.amount);
+  const changeExpenseHandler = () => {
+    console.log("Changes expense");
+    setPrice('$100');
   };
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails
-        amount={props.amount}
+        amount={price}
         location={props.location}
         title={props.title}
       />
-      <button onClick={deleteExpenseHandler}>Delete Expense</button>
+      <button onClick={changeExpenseHandler}>Change ExpenseAmount</button>
     </div>
   );
 };
