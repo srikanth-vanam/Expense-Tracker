4a447a56fb31e4a6751c806d7d8761e1d8767282
srikanth-vanam <B172320-@rgukt.ac.in>
2023-08-08 09:45:58 +0530

    added submit btn & handler

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -1,26 +1,59 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
-  const[editTitle,seteditTitle]=useState('');
-  const[editAmount,seteditAmount]=useState('');
-  const[editDate,seteditDate]=useState('');
+  const [editTitle, seteditTitle] = useState("");
+  const [editAmount, seteditAmount] = useState("");
+  const [editDate, seteditDate] = useState("");
+  // single useState for multiple states
 
-  const titleChangeHandler=(e)=>{
+  // const [userInput, setUserInput] = useState({
+  //   editTitle: "",
+  //   editAmount: "",
+  //   editDate: "",
+  // });
+
+  const titleChangeHandler = (e) => {
+    // using object destructuring
+
+    // setUserInput({
+    //   ...userInput,
+    //   editTitle: e.target.value,
+    // });
+
+    // best way of doing above work
+
+    // setUserInput((prevState) => {
+    //   return {
+    //     ...prevState,
+    //     editTitle: e.target.value,
+    //   };
+    // });
     seteditTitle(e.target.value);
-  }
-  const amountChangeHandler=(e)=>{
+  };
+  const amountChangeHandler = (e) => {
     seteditAmount(e.target.value);
-  }
-  const dateChangeHandler=(e)=>{
-      seteditDate(e.target.value);
-  }
+  };
+  const dateChangeHandler = (e) => {
+    seteditDate(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const expenseData = {
+      title:editTitle,
+      amount:editAmount,
+      date:new Date(editDate),
+    };
+    console.log(expenseData);
+  };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="form">
         <div className="form-input">
           <label for="expense-title">Expense title</label>
-          <input type="text"
+          <input
+            type="text"
             id="expense-title"
             name="expense-title"
             onChange={titleChangeHandler}
@@ -28,7 +61,8 @@ const ExpenseForm = () => {
         </div>
         <div className="form-input">
           <label for="expense-amount">Expense Amount</label>
-          <input type="text"
+          <input
+            type="text"
             id="expense-amount"
             name="expense-amount"
             onChange={amountChangeHandler}
@@ -36,14 +70,17 @@ const ExpenseForm = () => {
         </div>
         <div className="form-input">
           <label for="expense-date">Expense Date</label>
-          <input type="date"
+          <input
+            type="date"
             id="expense-date"
             name="expense-date"
             onChange={dateChangeHandler}
           ></input>
         </div>
         <div className="form-input">
-          <button type="submit">Form-Button</button>
+          <button type="submit">
+            Form-Button
+          </button>
         </div>
       </div>
     </form>
