e8aae58d0b5ccc400a78228025e700b60aee59f2
srikanth-vanam <B172320-@rgukt.ac.in>
2023-08-09 14:40:58 +0530

    added form toggling

--- src/components/ExpenseForm/ExpenseForm.js
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -56,7 +56,7 @@ const ExpenseForm = (props) => {
     setEnteredAmount('');
     setEnteredDate('');
   };
-
+  
   return (
     <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
@@ -90,6 +90,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type='button' onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
@@ -97,100 +98,3 @@ const ExpenseForm = (props) => {
 };
 
 export default ExpenseForm;
-
-// import { useState } from "react";
-// import "./ExpenseForm.css";
-// const ExpenseForm = () => {
-//   const [editTitle, seteditTitle] = useState("");
-//   const [editAmount, seteditAmount] = useState("");
-//   const [editDate, seteditDate] = useState("");
-//   // single useState for multiple states
-
-//   // const [userInput, setUserInput] = useState({
-//   //   editTitle: "",
-//   //   editAmount: "",
-//   //   editDate: "",
-//   // });
-
-//   const titleChangeHandler = (e) => {
-//     // using object destructuring
-
-//     // setUserInput({
-//     //   ...userInput,
-//     //   editTitle: e.target.value,
-//     // });
-
-//     // best way of doing above work
-
-//     // setUserInput((prevState) => {
-//     //   return {
-//     //     ...prevState,
-//     //     editTitle: e.target.value,
-//     //   };
-//     // });
-//     seteditTitle(e.target.value);
-//   };
-//   const amountChangeHandler = (e) => {
-//     seteditAmount(e.target.value);
-//   };
-//   const dateChangeHandler = (e) => {
-//     seteditDate(e.target.value);
-//   };
-
-//   const submitHandler = (e) => {
-//     e.preventDefault();
-//     const expenseData = {
-//       title:editTitle,
-//       amount:editAmount,
-//       date:new Date(editDate),
-//     };
-//     console.log(expenseData);
-//     // adding value to the form inputs. TWO-WAY-BINDING
-//     seteditTitle('');
-//     seteditAmount('');
-//     seteditDate('');
-//   };
-
-//   return (
-//     <form onSubmit={submitHandler}>
-//       <div className="form">
-//         <div className="form-input">
-//           <label for="expense-title">Expense title</label>
-//           <input
-//             type="text"
-//             id="expense-title"
-//             name="expense-title"
-//             value={editTitle}
-//             onChange={titleChangeHandler}
-//           ></input>
-//         </div>
-//         <div className="form-input">
-//           <label for="expense-amount">Expense Amount</label>
-//           <input
-//             type="text"
-//             id="expense-amount"
-//             name="expense-amount"
-//             value={editAmount}
-//             onChange={amountChangeHandler}
-//           ></input>
-//         </div>
-//         <div className="form-input">
-//           <label for="expense-date">Expense Date</label>
-//           <input
-//             type="date"
-//             id="expense-date"
-//             name="expense-date"
-//             value={editDate}
-//             onChange={dateChangeHandler}
-//           ></input>
-//         </div>
-//         <div className="form-input">
-//           <button type="submit">
-//             Form-Button
-//           </button>
-//         </div>
-//       </div>
-//     </form>
-//   );
-// };
-// export default ExpenseForm;
--- src/components/ExpenseForm/NewExpense.js
+++ src/components/ExpenseForm/NewExpense.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 const NewExpense = (props) => {
-    // function to get data from child comp ExpenseForm
-    const saveExpenseDataHandler=(userEnteredData)=>{
-        const ExpenseData={
-            ...userEnteredData,
-            id:Math.random().toString()
-        }
-        console.log(ExpenseData);
-        // passing expenseData from child to parent --> NE to app.js
-        props.onAddExpense(ExpenseData);
-    }
+  const [clicked, setClicked] = useState(false);
+  // function to get data from child comp ExpenseForm
+  const saveExpenseDataHandler = (userEnteredData) => {
+    const ExpenseData = {
+      ...userEnteredData,
+      id: Math.random().toString(),
+    };
+    console.log(ExpenseData);
+    // passing expenseData from child to parent --> NE to app.js
+    props.onAddExpense(ExpenseData);
+    // onsubmit making clicked false
+    setClicked(false);
+  };
+  const onClickHandler = () => {
+    setClicked(true);
+  };
+  const cancelHandler = () => {
+    setClicked(false);
+  };
   return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+    <div className="new-expense">
+      {!clicked && <button onClick={onClickHandler}>Add New Expense</button>}
+
+      {clicked && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={cancelHandler}
+        />
+      )}
     </div>
   );
 };
