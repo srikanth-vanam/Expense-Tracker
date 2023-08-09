bfba4e9b73a3b51346568f4e26e1985bf028f685
srikanth-vanam <B172320-@rgukt.ac.in>
2023-08-09 11:44:07 +0530

    Conditional Rendering + used display msg

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -11,21 +11,60 @@ const ExpenseItem = (props) => {
   const newExpenseArray = props.items.filter((exp) => {
     return exp.date.getFullYear().toString() === filteredYear;
   });
+  // using conditional rendering for display msg
+  let expenseContent=<p>No Expense-items to display here</p>;
+  // this is Jsx syntax
+  if(newExpenseArray.length > 0){
+    expenseContent=newExpenseArray.map((expense) => (
+      <ExpenseDetails
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        location={expense.location}
+      />
+    ))
+  }
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={changeFilterHandler}
       />
-      {newExpenseArray.map((expense) => (
-        <ExpenseDetails
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-          location={expense.location}
-        />
-      ))}
+      {/* one way of using conditional rendering */
+      /* {newExpenseArray.length === 0 && <p>No items to display</p>}
+      {newExpenseArray.length > 0 &&
+        newExpenseArray.map((expense) => (
+          <ExpenseDetails
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            location={expense.location}
+          />
+        ))} */
+      }
+      
+      {/* another way of using conditional rendering */
+      /* {newExpenseArray.length === 0 ? (
+        <p>No items to display</p>
+      ) : (
+        newExpenseArray.map((expense) => (
+          <ExpenseDetails
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            location={expense.location}
+          />
+        ))
+      )} */
+      }
+      {/* another way of using conditional rendering */}
+      {expenseContent}
+      {/* showing display msg on screen if there is only item */}
+      {newExpenseArray.length === 1?(<p>Only single Expense here. Please add more...</p>):(<p></p>)}
     </Card>
   );
 };
